70e53a841c7e75d722396ba6b3b4aef8c29ae009
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-11-06 10:00:26 -0500

    added refresh helper for dev

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,5 +1,10 @@
 (ns user
- (:require [figwheel-sidecar.repl-api :as ra]))
+ (:require
+  [figwheel-sidecar.repl-api :as ra]
+  [clojure.tools.namespace.repl :as repl]))
+
+(def refresh repl/refresh)
+
 
 (defn start-fw []
  (ra/start-figwheel!))
