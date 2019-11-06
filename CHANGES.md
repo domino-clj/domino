a0d0baaaae7569a923cb444e319661458da5d0d0
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-11-05 21:54:05 -0500

    updated benchmark test

--- CHANGES.md
+++ CHANGES.md
@@ -2,5 +2,11 @@
 
 - renamed `initialize!` to `initialize` since it's a pure function
 - updated the model parser to handle segments without an options map
+- inputs and outputs for events are now maps containing the keys
+  specified in the `:inputs` and `:outputs` vectors
+- event handler functions now must return a map with keys
+  matching the keys specified in the `:outputs`
+ - introduces `:pre` and `:post` conditions
+ - triggering events via tags or ids  
 
 
--- test/domino/benchmark.clj
+++ test/domino/benchmark.clj
@@ -5,23 +5,43 @@
     [criterium.core :as criterium]))
 
 (deftest ^:benchmark bench-initialize
-  (criterium/bench
-    (core/initialize
-      [{:inputs  [[:a]]
-        :outputs [[:b] [:c]]
-        :handler (fn [ctx [a] [b c]] [(+ a b) (+ a c)])}
-       {:inputs  [[:c]]
-        :outputs [[:d]]
-        :handler (fn [ctx [c] _] [(inc c)])}])))
+  (let [state (atom {})]
+    (criterium/bench
+      (core/initialize
+        {:model   [[:user {:id :user}
+                    [:first-name {:id :fname}]
+                    [:last-name {:id :lname}]
+                    [:full-name {:id :full-name}]]]
+         :effects [{:inputs  [:fname :lname :full-name]
+                    :handler (fn [_ {:keys [fname lname full-name]}]
+                               (swap! state assoc
+                                      :first-name fname
+                                      :last-name lname
+                                      :full-name full-name))}]
+         :events  [{:inputs  [:fname :lname]
+                    :outputs [:full-name]
+                    :handler (fn [_ {:keys [fname lname]} _]
+                               {:full-name (or (when (and fname lname) (str lname ", " fname))
+                                               fname
+                                               lname)})}]}))))
 
 (deftest ^:benchmark bench-transact
   (let [ctx (core/initialize
-              [{:inputs  [[:a]]
-                :outputs [[:b] [:c]]
-                :handler (fn [ctx [a] [b c]] [(+ a b) (+ a c)])}
-               {:inputs  [[:c]]
-                :outputs [[:d]]
-                :handler (fn [ctx [c] _] [(inc c)])}])]
+              {:model   [[:f {:id :f}
+                          [:a {:id :a}]
+                          [:b {:id :b}]
+                          [:c {:id :c}]
+                          [:d {:id :d}]]]
+               :events  [{:inputs  [:a :b]
+                          :outputs [:c]
+                          :handler (fn [_ {:keys [a b]} _]
+                                     {:c "C"})}
+                         {:inputs  [:b :c]
+                          :outputs [:d]
+                          :handler (fn [_ {:keys [b c]} _]
+                                     {:d "D"})}
+                         {:inputs  [:d]
+                          :outputs [:a]
+                          :handler (fn [_ {:keys [d]} _]
+                                     {:a "A"})}]})]
     (criterium/bench (core/transact ctx [[[:a] 1] [[:b] 1]]))))
-
-
